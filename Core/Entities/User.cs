5caf91a97b8aa333265a269d12f4b00b57da2a8e
Bruno Carvalho <brunocarvalhodesa@gmail.com>
2021-06-08 00:27:40 -0300

    first commit

---
+++ Core/Entities/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Core.Entities
+{
+    public class User : BaseEntity
+    {
+        public User(string fullName, decimal height, decimal weight, DateTime bithDate) : base()
+        {
+            FullName = fullName;
+            Height = height;
+            Weight = weight;
+            BithDate = bithDate;
+        }
+
+        public string FullName { get; private set; }
+
+        public decimal Height { get; private set; }
+
+        public decimal Weight { get;private set; }
+
+        public DateTime BithDate { get; private set; }
+
+        public IEnumerable<Meal> Meals { get; private set; }
+
+        public void Update(decimal height, decimal weight)
+        {
+            Height = height;
+            Weight = weight;
+        }
+    }
+}
